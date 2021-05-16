96ded22115596f09446403f49796a665dd96180d
Anthony Hernandez <anthonyrhernandez6@gmail.com>
2021-05-16 13:59:46 -0400

    DataBase2

---
+++ DataBase2/DataBase2/fmprueba.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="fmprueba.aspx.cs" Inherits="DataBase2.fmprueba" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Button ID="ButtonSubirInformacion" runat="server" Height="119px" OnClick="ButtonSubirInformacion_Click" Text="Cargar info CSV" Width="241px" />
+    </form>
+</body>
+</html>
---
+++ DataBase2/DataBase2/fmprueba.aspx.cs
@@ -0,0 +1,48 @@
+﻿using DataBase2.Clases.Archivos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataBase2
+{
+    public partial class fmprueba : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cargarArchivoExterno()
+        {
+            string fuente = @"C:\Users\antho\OneDrive\Escritorio\BaseDeDatos\crudDB.csv";
+            ClsArchivo ar = new ClsArchivo();
+            //obtener todo el archivo, linea por linea dentro de un arreglo
+            string[] ArregloNotas = ar.LeerArchivo(fuente);
+            string sentencia_sql = "";
+            int Numerolinea = 0;
+
+            ClsConexion cn = new ClsConexion();
+
+            foreach (string linea in ArregloNotas)
+            {
+                //obtener datos
+                String[] datos = linea.Split(';');
+                if (Numerolinea > 0)
+                {
+                    sentencia_sql = sentencia_sql + $"insert into tb_alumnos values({datos[0]},'{datos[1]}',{datos[2]},{datos[3]},{datos[4]},{datos[5]},'{datos[6]}');\n ";
+                }
+                Numerolinea++;
+            }
+
+            cn.EjecutaSQLDirecto(sentencia_sql);
+        }
+
+        protected void ButtonSubirInformacion_Click(object sender, EventArgs e)
+        {
+            cargarArchivoExterno();
+        }
+    }
+}
